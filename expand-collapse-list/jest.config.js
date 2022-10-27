117fa626218e1fe5f53abf11a4d0b5bcb0af6324
Shu Etsu <yue.zhu@rakuten.com>
2022-10-27 23:03:47 +0900

    remove it

--- expand-collapse-list/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    clearMocks: true,
-    setupFilesAfterEnv: ['regenerator-runtime/runtime'],
-    testPathIgnorePatterns: [
-        "/node_modules/",
-    ],
-    useStderr: true
-};
